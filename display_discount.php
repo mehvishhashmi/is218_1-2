956cdbb05aedf76091e52ff2b94ea6ed7c9e4d19
mehvishhashmi <mh449@njit.edu>
2017-02-10 10:16:37 -0500

    adding display_discount.php

--- display_discount.php
+++ display_discount.php
@@ -1,36 +1,28 @@
-<?php
-	$product_description = ( 'product_description');
-	$list_price = ('list_price');
-	$discount_percent = ('discount_percent');
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Product Discount Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
+
 <body>
+    <main>
+	<h1>This page is under construction</h1>
 
-  <main>
-         <h1>This page is under construction</h1>
+	<label>Product Description:</label>
+	<span><?php echo ''; ?></span><br>
 
-	         <label>Product Description:</label>
-		 <span><?php echo ($product_description); ?></span><br>
+	<label>List Price:</label>
+	<span><?php echo ''; ?></span><br>
+	
+	<label>Standard Discount:</label>
+	<span><?php echo''; ?></span><br>
 
-		 <label>List Price:</label>
-		 <span><?php echo ($list_price); ?></span><br>
+	<label>Discount Amount:</label>
+	<span><?php echo ''; ?></span><br>
 
-		 <label>Standard Discount:</label>
-		 <span><?php echo ($discount_percent); ?></span><br>
-
-		 <label>Discount Amount:</label>
-		 <span><?php echo ''; ?></span><br>
-
-		 <label>Discount Price:</label>
-		 <span><?php echo ''; ?></span><br>
+	<label>Discount Price:</label>
+	<span><?php echo ''; ?></span><br>
 
     </main>
 </body>
